920878f3baf7ca4c548c9bd77bf3daf25a2c8512
Carlos Alberto <clsalberto@outlook.com>
2020-03-08 01:00:46 -0300

    feat: add initial commit
    Preparing initial configurations for nodejs projects

---
+++ src/app.js
@@ -0,0 +1,50 @@
+import 'dotenv/config';
+
+import cors from 'cors';
+import express from 'express';
+import 'express-async-errors';
+import helmet from 'helmet';
+import Youch from 'youch';
+
+import routes from './routes';
+
+// import './database';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+    this.exceptionHandler();
+  }
+
+  middlewares() {
+    this.server.use(helmet());
+    this.server.use(
+      cors({
+        origin: process.env.FRONT_URL,
+      })
+    );
+
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
+    });
+  }
+}
+
+export default new App().server;
